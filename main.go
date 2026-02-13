45f53418e3ebd96e36258e2eb4f1f8a72f01eb41
ll1bles4 <ul.alexey.mokeev@gmail.com>
2026-02-13 22:13:30 +0400

    Step 1: Server starting with chi

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Заебись четка")
+	})
+
+	http.ListenAndServe(":8000", r)
+}
